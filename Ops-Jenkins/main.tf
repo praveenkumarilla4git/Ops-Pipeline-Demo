1b6f7b7eccddf9d401410030f4f1bc64979731a4
praveenkumarilla4git <praveenkumarilla@gmail.com>
2025-12-25 21:28:25 +0530

    Adding all the files

--- Ops-Jenkins/main.tf
+++ Ops-Jenkins/main.tf
@@ -55,6 +55,14 @@ resource "aws_security_group" "jenkins_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Allow App Traffic"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
